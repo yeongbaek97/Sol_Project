778d3061876f5dd09f799a6985aa175c3454ccab
hj <h02j18@gmail.com>
2022-02-08 17:28:29 +0900

    update DAO, Controller, Bean

---
+++ src/main/java/com/sol/pj/hj/Accom.java
@@ -0,0 +1,77 @@
+package com.sol.pj.hj;
+
+import java.math.BigDecimal;
+
+public class Accom {
+
+	private String title;
+	private String addr;
+	private String img;
+	private String classifi;
+	private BigDecimal mapX;
+	private BigDecimal mapY;
+	
+	public Accom() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Accom(String title, String addr, String img, String classifi, BigDecimal mapX, BigDecimal mapY) {
+		super();
+		this.title = title;
+		this.addr = addr;
+		this.img = img;
+		this.classifi = classifi;
+		this.mapX = mapX;
+		this.mapY = mapY;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getClassifi() {
+		return classifi;
+	}
+
+	public void setClassifi(String classifi) {
+		this.classifi = classifi;
+	}
+
+	public BigDecimal getMapX() {
+		return mapX;
+	}
+
+	public void setMapX(BigDecimal mapX) {
+		this.mapX = mapX;
+	}
+
+	public BigDecimal getmapY() {
+		return mapY;
+	}
+
+	public void setmapY(BigDecimal mapY) {
+		this.mapY = mapY;
+	}
+	
+	
+}
---
+++ src/main/java/com/sol/pj/hj/GcController.java
@@ -0,0 +1,46 @@
+package com.sol.pj.hj;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class GcController {
+	
+	@Autowired
+	private GoCampingDAO gDAO;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		
+		return "Accommodation/AccomIndex";
+	}
+	
+	@RequestMapping(value = "goto.gocamping", method = RequestMethod.GET)
+	public String goTogocamping() {
+		
+		return "Accommodation/GoCamping";
+	}
+	
+	@RequestMapping(value = "goto.accom", method = RequestMethod.GET)
+	public String goToaccom() {
+		
+		return "Accommodation/Accom";
+	}
+	
+	@RequestMapping(value = "json.get", method = RequestMethod.GET,
+			produces = "application/json; charset=utf-8")
+	public String getMenuJSON(GoCamping gc, HttpServletRequest request) throws IOException, ParseException {
+		List<GoCamping> mm = gDAO.gocampingJSON(request);
+		request.setAttribute("gocamping", mm);
+		return "Accommodation/GoCamping";
+	}
+	
+}
