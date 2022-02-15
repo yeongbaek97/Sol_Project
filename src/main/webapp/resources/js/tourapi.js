1633a813bf1e47f10a8b4cb6b23a0b01fe8a2692
hyunsu <gustndht10@naver.com>
2022-02-15 18:08:41 +0900

    병합

---
+++ src/main/webapp/resources/js/tourapi.js
@@ -0,0 +1,45 @@
+/*function tourapia(){
+
+	$(function() {
+		$.ajax({
+			// 서버로 보낼 주소 입력
+			url : "tour.list",
+			// type 설정
+			type : 'GET',
+			// 요청한 데이터 형식
+			dataType : 'json',
+			// 전송할 데이터
+			data : {},
+			// 성공적으로 응답을 받으면
+			success : function(data) {
+				// 전송에 성공하면 실행될 코드
+				
+				console.log(data);
+				
+			}
+			
+		});
+		
+});
+
+}
+
+
+ * 상세정보 이동
+ * http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailIntro&contentId
+
+function aa(contentid) {
+	location.href="tour.list.detail&contentid";
+}*/
+
+/*function bbb() {
+	$("#dd").mouseover(function() {
+		
+		$("#dddd").dialog("open");
+	});
+};
+
+$(function() {
+	
+	bbb();
+});*/
